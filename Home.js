7fdad30a027f1213faa0330aeb7a8b55f9480998
Ofodum Joseph <josephofodum@gmail.com>
2022-12-22 03:46:34 +0100

    Add files via upload
    Here with the help of filter we were able to reuse our component while outputting different datas.

--- Home.js
+++ Home.js
@@ -3,14 +3,16 @@ import BlogList from './BlogList';
 
 const Home  = () => {
  const [blogs, setBlogs] = useState([
-    {title: 'My new website', body: 'bulabalu ....', author: 'meggie', id: 1},
+    {title: 'My new website', body: 'bulabalu ....', author: 'joe', id: 1},
     {title: 'Marriage ceremony', body: 'lorem ipsum...', author: 'favor', id: 2},
-    {title: 'fashon trends', body:'lorem ipsum', author: 'courage', id: 3}
+    {title: 'fashon trends', body:'lorem ipsum', author: 'joe', id: 3}
  ]);
     
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title='Result Oriented engineer!' />
+            <BlogList blogs={blogs} title='All blogs' />
+            <BlogList blogs={blogs.filter((blog)=> blog.author === 'joe')} title='joe' />
+
            
         </div>
      );
